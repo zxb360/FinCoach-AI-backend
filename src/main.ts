c943c7eab455426b5a1b3c7d5408d67395354424
jaeder <zxb360@gmail.com>
2026-06-29 19:52:44 -0300

    cors alterado

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'https://projeto-financa-dio-1.onrender.com/',
+      'http://localhost:5174',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   // const corsOptions: CorsOptions = {
   //   origin: true,
   //   credentials: true,
   // };
-  app.enableCors();
+  // app.enableCors();
 
   await app.listen(process.env.PORT ?? 3000);
 }
