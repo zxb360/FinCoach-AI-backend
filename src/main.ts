c0bfc93518530ee272245752c0ace4c8608342dc
jaeder <zxb360@gmail.com>
2026-06-29 21:05:09 -0300

    alteração refatoração do código

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
-      'https://projeto-financa-dio-1.onrender.com/assistente-ai',
+      'https://projeto-financa-dio-1.onrender.com/',
       'http://localhost:5174',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
