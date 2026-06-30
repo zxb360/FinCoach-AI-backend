52552d5987034872ca895a0fa48f4b0e32268fee
jaeder <zxb360@gmail.com>
2026-06-29 21:11:52 -0300

    origin

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'https://projeto-financa-dio-1.onrender.com/',
-      'http://localhost:5174',
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
+    // origin: [
+    //   'https://projeto-financa-dio-1.onrender.com/',
+    //   'http://localhost:5174',
+    // ],
+    origin: true,
+    // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    // credentials: true,
   });
   // const corsOptions: CorsOptions = {
   //   origin: true,
